29eb58a242a53c1c1527a4fac70fcdf5def9e7e6
Mourice-Adero <aderomourice7@gmail.com>
2022-11-28 23:25:45 +0300

    added AddAdminEmployees form

--- packages/ui/src/views/admin/AdminEmployees.tsx
+++ packages/ui/src/views/admin/AdminEmployees.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAppSelector } from '../../state/hooks';
 
+import AddAdminEmployees from './AddAdminEmployees';
 import Table, { TableColumnProps } from '../../components/Table';
 
 const cols: TableColumnProps[] = [
@@ -51,11 +52,30 @@ const cols: TableColumnProps[] = [
 
 const AdminEmployees = () => {
 	const { employees } = useAppSelector((state) => state.employees);
+	const [addingEmployees, setAddingEmployees] =
+	React.useState<boolean>(false);
+const closeOpenModal = () => {
+	setAddingEmployees(false);
+};
 
 	return (
 		<div>
-			<h1>Employees</h1>
+			<div className="px-4 py-1 my-4 border mx-2 flex justify-between">
+				<h1>Employees</h1>
+				<button
+					onClick={(e) => {
+						e.stopPropagation();
+						setAddingEmployees(true);
+					}}
+					className="bg-blue-700 text-white px-4 py-2 rounded"
+				>
+					<span className="material-icons">add new Employee</span>
+				</button>
+			</div>
 			<div>
+			{addingEmployees && (
+					<AddAdminEmployees closeModal={closeOpenModal} />
+				)}
 				<Table columns={cols} rows={employees} />
 			</div>
 		</div>
