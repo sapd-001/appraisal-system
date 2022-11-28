1e6e5c55466eccd3b7496e77319d3c8f90269ca7
Felix Orinda <forinda82@gmail.com>
2022-11-28 17:46:46 +0300

    Add role on dashboard header

--- packages/ui/src/components/LayoutHeader.tsx
+++ packages/ui/src/components/LayoutHeader.tsx
@@ -1,9 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import profile from './../assets/profile.png';
+import { useAppSelector } from '../state/hooks';
+
 import { faBars, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const LayoutHeader = () => {
+	const { user } = useAppSelector((state) => state.root.user);
+	
 	return (
 		<div className="px-6 py-4 bg-blue-600 w-full text-white flex justify-between items-center sticky top-0">
 			<div className="flex items-center gap-3">
@@ -13,8 +17,8 @@ const LayoutHeader = () => {
 			<div className="flex items-center gap-2">
 				<img src={profile} alt="" className="h-10 w-10 rounded-full" />
 				<div>
-					<button className='text-xl'>
-						Administrator <FontAwesomeIcon icon={faChevronDown} />
+					<button className="text-xl">
+						{user?.role} <FontAwesomeIcon icon={faChevronDown} />
 					</button>
 				</div>
 			</div>
--- packages/ui/src/views/admin/AdminDashboard.tsx
+++ packages/ui/src/views/admin/AdminDashboard.tsx
@@ -1,22 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import dashboardSummary from '../../data/adminDashboardSummary';
+// import { useAppSelector } from '../../state/hooks';
 
 const AdminDashboard = () => {
+	// const { user } = useAppSelector((state) => state.root.user);
+	
 	return (
 		<div className="flex flex-col gap-4">
-			<div className='border-b border-slate-400 py-6 px-2'>
-				<h1 className='text-4xl font-bold'>Welcome to the admin dashboard</h1>
+			<div className="border-b border-slate-400 py-6 px-2">
+				<h1 className="text-4xl font-bold">
+					Welcome to the admin dashboard
+				</h1>
 			</div>
 			<div className="grid grid-cols-[repeat(auto-fit,minmax(360px,1fr))] p-4 gap-4">
 				{dashboardSummary.map((sum) => (
-					<div key={sum.title} className="flex items-center gap-4 border border-slate-400 justify-between p-10 shadow-[14px_-6px_62px_-4px_rgba(199,199,199,1),14px_-6px_62px_-4px_rgba(199,199,199,1)] rounded-md hover:scale-105 transition-all ease-linear duration-700 cursor-pointer">
-						<div className='flex flex-col gap-4'>
-							<h2 className='font-bold uppercase text-6xl'>{sum.count}</h2>
-							<p className='text-lg'>{sum.title}</p>
+					<div
+						key={sum.title}
+						className="flex items-center gap-4 border border-slate-400 justify-between p-10 shadow-[14px_-6px_62px_-4px_rgba(199,199,199,1),14px_-6px_62px_-4px_rgba(199,199,199,1)] rounded-md hover:scale-105 transition-all ease-linear duration-700 cursor-pointer"
+					>
+						<div className="flex flex-col gap-4">
+							<h2 className="font-bold uppercase text-6xl">
+								{sum.count}
+							</h2>
+							<p className="text-lg">{sum.title}</p>
 						</div>
 						<div>
-							<FontAwesomeIcon icon={sum.icon} size="5x" className='text-slate-400'/>
+							<FontAwesomeIcon
+								icon={sum.icon}
+								size="5x"
+								className="text-slate-400"
+							/>
 						</div>
 					</div>
 				))}
